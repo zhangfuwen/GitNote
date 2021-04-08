9651bd73645c4a21fafdcea656be2135f67b6c16
zhangfuwen <Dean.Sinaean@gmail.com>
2021-04-08 06:37:06 +0000

    update vim

--- env_setup.sh
+++ env_setup.sh
@@ -11,6 +11,9 @@ function download_and_run()
     sh -c "$(wget $url -O -)"
 }
 
+# aarch64 x86_64
+architecture=$(lscpu | awk '/Architecture:/{print $2}') 
+
 function setup_vim()
 {
 	download_and_run https://gitee.com/zhangfuwen/GitNote/raw/master/vim/vim_setup.sh
--- vim/vim_setup.sh
+++ vim/vim_setup.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-$SUDO apt install vim
+function download_nvim_x86()
+{
+    echo "try install neovim"
+    mkdir ~/bin/
+    wget -O ~/bin/nvim-nightly.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz 
+    tar xvzf ~/bin/nvim-nightly.tar.gz -C ~/bin/
+    echo "export PATH=~/bin/nvim-linux64/bin:$PATH" >> ~/.bashrc
+    source ~/.bashrc 
+    rm nvim-linux64.tar.gz  -C ~/bin/
+    $VIM=nvim
+}
+
+function download_nvim_arm64()
+{
+    echo "try install vim"
+    $SUDO apt-get install -y vim || $SUDO yum install -y vim
+    $VIM=vim
+}
+
+if [[ $architecture == "aarch64" ]]; then
+    download_nvim_arm64
+else if [[ $architecture == "x86_64" ]]; then
+    download_nvim_x86
+fi
+
+cat > ~/.config/nvim/init.vim << EOF
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc
+EOF
+
+# $SUDO apt install vim
+
 curl -fLo ~/.vimrc --create-dirs https://gitee.com/zhangfuwen/GitNote/raw/master/vim/vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim -E -c PlugInstall -c q
+$VIM -E -c PlugInstall -c q
 
 $SUDO apt-get install -y ripgrep cppman ctags || $SUDO yum install -y ripgrep cppman ctags || $SUDO dnf install -y ripgrep cppman ctags
