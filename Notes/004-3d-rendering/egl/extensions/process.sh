3a89fdeab3069002769a4ebd79c2e7c36931bf89
zhangfuwen <zhangfuwen@foxmail.com>
2022-02-14 18:05:08 +0800

    update index

--- Notes/004-3d-rendering/egl/extensions/process.sh
+++ Notes/004-3d-rendering/egl/extensions/process.sh
@@ -10,21 +10,52 @@ function trimTrailing()
 	echo "${1}" | sed -e 's/\ *$//g'
 }
 
-for file in $(find . -name "*.txt"); do
-	title=$(cat $file | head -n 3 | tail -n 1)
-	title=$(trimLeading $title)
+function createMdFromTxt()
+{
+    for file in $(find . -name "*.txt"); do
+        title=$(cat $file | head -n 3 | tail -n 1)
+        title=$(trimLeading $title)
 
-	newfile="${file%.txt}.md"
-	echo $newfile
+        newfile="${file%.txt}.md"
+        echo $newfile
 
-	echo "# $title" > $newfile
-	echo "" >> $newfile
-	cat $file >> $newfile
+        echo "# $title" > $newfile
+        echo "" >> $newfile
+        cat $file >> $newfile
 
-done
+    done
 
-for d in $(find . -type d); do
-    echo "# $d" > $d/index.md
-done
+}
+
+function createDirIndex()
+{
+    for d in $(find . -type d); do
+        echo "# $d" > $d/index.md
+    done
+}
+
+
+function createIndex()
+{
+    echo '---
+
+has_children: true
+
+---
+
+# EGL' > ../index.md
+
+    files=$(find . -name "*.txt")
+    for f in $files; do
+        title=$(cat $f | head -n 3 | tail -n 1)
+        title=$(trimLeading $title)
+        echo "[$title](${f%.txt}.html)" >> ../index.md
+        echo "[$title](${f%.txt}.html)"
+
+
+    done
+}
+
+createIndex
 
 # for file in $(find . -name "*.txt"); do mv -- "$file" "${file%.txt}.md"; done
