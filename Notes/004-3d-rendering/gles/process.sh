74117e7629317cb9acda0e0b5c993251874d56b5
zhangfuwen <zhangfuwen@foxmail.com>
2022-02-15 12:57:12 +0800

    addlist

--- Notes/004-3d-rendering/gles/process.sh
+++ Notes/004-3d-rendering/gles/process.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 function getText() {
-    cat list.txt| egrep -o "http.*txt" | xargs wget --parent
-}
-
-function copyAsMd() {
-    files=$(find . -name "*.txt")
-    for file in $files; do
-        md_name=${file%.txt}.md
-        cp $file $md_name
-    done
+    if [[ ! -d texts ]]; then
+        mkdir texts
+    fi
+    cd texts
+    cat ../original_list.md| egrep -o "http.*txt" | xargs wget --parent
+    cd -
 }
 
 
@@ -25,6 +22,7 @@ function trimTrailing()
 
 function createMdFromTxt()
 {
+    cd texts
     for file in $(find . -name "*.txt"); do
         title=$(cat $file | head -n 3 | tail -n 1)
         title=$(trimLeading $title)
@@ -37,7 +35,11 @@ function createMdFromTxt()
         cat $file >> $newfile
 
     done
-
+    cd -
+    if [[ ! -d extensions ]]; then
+        mkdir extensions
+    fi
+    find texts -name "*.md" | xargs -I {} mv {} extensions    files=$(find ../texts/ -name "*.md")
 }
 
 function createDirIndex()
@@ -50,15 +52,20 @@ function createDirIndex()
 
 function createIndex()
 {
-    echo '# List' > ../list.md
-    echo "" >> ../list.md
+    echo "---" > list.md
+    echo "" >> list.md
+    echo "has_children: true" >> list.md
+    echo "" >> list.md
+    echo "---" >> list.md
+    echo '# List' >> list.md
+    echo "" >> list.md
 
     lines=()
-    files=$(find . -name "*.txt")
+    files=$(find extensions -name "*.md")
     for f in $files; do
-        title=$(cat $f | head -n 3 | tail -n 1)
-        title=$(trimLeading $title)
-        lines+=("[$title](extensions/${f%.txt}.html)")
+        title=$(basename $f)
+        title=${title%.md}
+        lines+=("[$title](${f%.md}.html)")
     done
 
 
@@ -67,13 +74,46 @@ function createIndex()
 #    for a in ${sorted[@]}; do echo $a; done
 
     for line in ${sorted[@]}; do
-        echo $line >> ../list.md
-        echo "" >> ../list.md
+        echo $line >> list.md
+        echo "" >> list.md
+    done
+}
+
+function getEnc()
+{
+    encguess $1 | awk '{print $2}'
+}
+
+function conv2utf8()
+{
+    test -d extensions_new || mkdir extensions_new
+
+    files=$(find extensions -name "*.md")
+    echo "" > conv_failed_files
+    echo "" > conv_done_files
+    echo "" > conv_copy_files
+    for file in $files; do
+        newpath=extensions_new/$(basename $file)
+
+        enc=$(getEnc $file)
+        if [[ $enc =~ ^(US-ASCII|UTF-8)$ ]]; then
+            echo "file $file $enc" >> conv_copy_files
+            cp $file $newpath
+        else
+            if iconv -f $enc -t utf8 $file -o $newpath; then
+                echo "$file $enc done" >> conv_done_files
+            else
+                echo $file >> conv_failed_files
+
+            fi
+        fi
     done
 }
 
 #getText
 #createMdFromTxt
+
+#conv2utf8
 
 createIndex
 
